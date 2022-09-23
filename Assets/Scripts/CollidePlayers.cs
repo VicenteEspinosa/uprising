13cb083e2fc3130dea0be9d311be51bd66ae7dce
Vicente Espinosa <vnespinosa@uc.cl>
2022-09-23 11:22:48 -0300

    add all transitions

---
+++ Assets/Scripts/CollidePlayers.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollidePlayers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Detective" || collision.gameObject.tag == "Firefighter")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
