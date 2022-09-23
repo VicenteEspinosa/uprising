89cccbf7179966eca193c82825533dd74f9485b2
kipasten <pasten.ig@uc.cl>
2022-09-23 11:04:15 -0300

    follow closest player

--- Assets/SimpleNav.cs
+++ Assets/SimpleNav.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class SimpleNav : MonoBehaviour
 {
     public GameObject player;
+    public GameObject player2;
+    private GameObject moveto;
     public float speed;
     public float sightRange;
 
-    private float distance;
+    private float distance = 100000;
+    private float distance2 = 100000;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,18 +20,42 @@ public class SimpleNav : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {   
+        CheckDistances();
+
+        if(distance<distance2)
+        {          
+            MoveTowards(player);
+        } else if (distance2 < distance){
+            MoveTowards(player2);
+        }
+        
+    }
+
+    void CheckDistances()
     {
-        if (player != null)
-        {
+        if (player != null){
             distance = Vector2.Distance(transform.position, player.transform.position);
-            Vector2 direction = player.transform.position - transform.position;
+        } else {
+            distance = 100000;
+        }
+            
+        if (player2 != null){
+            distance2 = Vector2.Distance(transform.position, player2.transform.position);
+        } else {
+            distance2 = 100000;
+        }
+    }
+
+    void MoveTowards(GameObject jugador)
+    {
+        Vector2 direction = jugador.transform.position - transform.position;
             direction.Normalize();
             float angle = Mathf.Atan2(direction.y, direction.x)* Mathf.Rad2Deg;
             
             if(distance < sightRange)
-            {   transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            {   transform.position = Vector2.MoveTowards(this.transform.position, jugador.transform.position, speed * Time.deltaTime);
                 transform.rotation = Quaternion.Euler(Vector3.forward * angle);
             }
-        }
     }
 }
