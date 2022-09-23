9990417fc6c925883bd6ad939c52654a20ab3b10
Vicente Espinosa <vnespinosa@uc.cl>
2022-09-23 03:08:02 -0300

    mutant takes damage from players

--- Assets/Scripts/Mutant/EnemyCollision.cs
+++ Assets/Scripts/Mutant/EnemyCollision.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollideFirefighter : MonoBehaviour
+public class EnemyColision : MonoBehaviour
 {
+    public float health;
+    public float bulletDamage;
+    public float axeDamage;
+
     bool isCollidingWithFirefighter = false;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,14 +22,19 @@ public class CollideFirefighter : MonoBehaviour
     void Update()
     {
         if (!InteractDoor.isInteracting && Input.GetAxis("Atack Firefighter") != 0 && isCollidingWithFirefighter)
-            {
-                Destroy(gameObject);
-            }
+        {
+            TakeDamage(axeDamage);
+        }
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Firefighter")
+        if (collision.gameObject.tag == "Bullet")
+        {
+            TakeDamage(bulletDamage);
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.tag == "Firefighter")
         {
             isCollidingWithFirefighter = true;
         }
@@ -33,6 +45,15 @@ public class CollideFirefighter : MonoBehaviour
         if (collision.gameObject.tag == "Firefighter")
         {
             isCollidingWithFirefighter = false;
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
