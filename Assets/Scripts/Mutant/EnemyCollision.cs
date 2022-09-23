ebdfb784d77294a168b2c0179e29873e5dbf22d1
Vicente Espinosa <vnespinosa@uc.cl>
2022-09-23 03:21:56 -0300

    players take damage from mutant

--- Assets/Scripts/Mutant/EnemyCollision.cs
+++ Assets/Scripts/Mutant/EnemyCollision.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyColision : MonoBehaviour
+public class EnemyCollision : MonoBehaviour
 {
     public float health;
     public float bulletDamage;
