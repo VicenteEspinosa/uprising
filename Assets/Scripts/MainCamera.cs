bf7aa8ca3095ef88b45e52484ac4417415f23912
Seba <snburgos@uc.cl>
2022-11-03 00:33:28 -0300

    Fix: camera fixed zoom, detective points mouse

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -9,16 +9,16 @@ public class MainCamera : MonoBehaviour
     public List<Transform> targets;
     public Vector3 offset;
     public float smoothTime = .1f;
-    public float minZoom = 130f;
-    public float maxZoom = 60f;
-    public float zoomLimiter = 20f;
+    // public float minZoom = 130f;
+    // public float maxZoom = 60f;
+    // public float zoomLimiter = 20f;
 
     private Vector3 velocity;
-    private Camera cam;
+    // private Camera cam;
 
     private void Start()
     {
-        cam = GetComponent<Camera>();
+        // cam = GetComponent<Camera>();
     }
 
     private void LateUpdate()
@@ -40,14 +40,14 @@ public class MainCamera : MonoBehaviour
         }
 
         Move(bounds);
-        Zoom(bounds);
+        // Zoom(bounds);
     }
 
-    private void Zoom(Bounds bounds)
-    {
-        float newZoom = Mathf.Lerp(maxZoom, minZoom, Mathf.Max(bounds.size.x, bounds.size.y) / zoomLimiter);
-        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
-    }
+    // private void Zoom(Bounds bounds)
+    // {
+    //     float newZoom = Mathf.Lerp(maxZoom, minZoom, Mathf.Max(bounds.size.x, bounds.size.y) / zoomLimiter);
+    //     cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+    // }
 
     private void Move(Bounds bounds)
     {
