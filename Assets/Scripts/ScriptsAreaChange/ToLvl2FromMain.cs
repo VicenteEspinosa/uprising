7e913a76eba5c42b78f28810edb29ff0dab5ac55
Sebastián Burgos <snburgos@uc.cl>
2022-11-25 00:27:31 -0300

    Load Scenes

--- Assets/Scripts/ScriptsAreaChange/ToLvl2FromMain.cs
+++ Assets/Scripts/ScriptsAreaChange/ToLvl2FromMain.cs
@@ -5,7 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class ToLvl2FromMain : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other){
-        SceneManager.LoadScene("Level2");
+    public GameObject cluePrefab;
+
+    private GameObject clueText;
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Detective") || collision.gameObject.CompareTag("Firefighter"))
+        {
+            if (GameObject.FindGameObjectsWithTag("Clue").Length > 0)
+            {
+                clueText = Instantiate(cluePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            }
+            else
+            {
+                SceneManager.LoadScene("Level2");
+            }
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (clueText)
+        {
+            Destroy(clueText);
+        }
     }
 }
--- Assets/Scripts/ScriptsAreaChange/WinFromLvl2.cs
+++ Assets/Scripts/ScriptsAreaChange/WinFromLvl2.cs
@@ -5,7 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class WinFromLvl2 : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other){
-        SceneManager.LoadScene("Spawn");
+    public GameObject cluePrefab;
+
+    private GameObject clueText;
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Detective") || collision.gameObject.CompareTag("Firefighter"))
+        {
+            if (GameObject.FindGameObjectsWithTag("Clue").Length > 0)
+            {
+                clueText = Instantiate(cluePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            }
+            else
+            {
+                SceneManager.LoadScene("Spawn");
+            }
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (clueText)
+        {
+            Destroy(clueText);
+        }
     }
 }
