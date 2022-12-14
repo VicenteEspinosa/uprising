7603197dfd37db850f03020d492e010ec093d40c
Sebastián Burgos <snburgos@uc.cl>
2022-12-14 00:46:30 -0300

    Pause Menu Finished

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool gameIsPause = false;
-
     public GameObject pauseMenuUI;
+    public AudioMixer audioMixer;
 
     // Update is called once per frame
     void Update()
@@ -35,5 +37,17 @@ public class PauseMenu : MonoBehaviour
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         gameIsPause = true;
+    }
+
+    public void LoadMenu()
+    {
+        gameIsPause = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
     }
 }
